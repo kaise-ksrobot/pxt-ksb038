521c9e3a767d7c8e6cc3b8830581c2adbe7fe786
ksrobot <arthur@kaise.com.tw>
2018-04-22 12:58:21 +0800

    initial push

---
+++ main.ts
@@ -0,0 +1,279 @@
+/**
+ * KSB038
+ */
+ 
+//% weight=10 color=#0000fa icon=""
+namespace KSB038 {
+    let _DEBUG: boolean = false
+    const debug = (msg: string) => {
+        if (_DEBUG === true) {
+            serial.writeLine(msg)
+        }
+    }
+
+    const MIN_CHIP_ADDRESS = 0x40
+    const MAX_CHIP_ADDRESS = MIN_CHIP_ADDRESS + 62
+    const chipResolution = 4096;
+    const PrescaleReg = 0xFE //the prescale register address
+    const modeRegister1 = 0x00 // MODE1
+    const modeRegister1Default = 0x01
+    const modeRegister2 = 0x01 // MODE2
+    const modeRegister2Default = 0x04
+    const sleep = modeRegister1Default | 0x10; // Set sleep bit to 1
+    const wake = modeRegister1Default & 0xEF; // Set sleep bit to 0
+    const restart = wake | 0x80; // Set restart bit to 1
+    const allChannelsOnStepLowByte = 0xFA // ALL_LED_ON_L
+    const allChannelsOnStepHighByte = 0xFB // ALL_LED_ON_H
+    const allChannelsOffStepLowByte = 0xFC // ALL_LED_OFF_L
+    const allChannelsOffStepHighByte = 0xFD // ALL_LED_OFF_H
+    const PinRegDistance = 4
+    const channel0OnStepLowByte = 0x06 // LED0_ON_L
+    const channel0OnStepHighByte = 0x07 // LED0_ON_H
+    const channel0OffStepLowByte = 0x08 // LED0_OFF_L
+    const channel0OffStepHighByte = 0x09 // LED0_OFF_H
+
+
+    export enum PinNum {
+        Pin0 = 0,
+        Pin1 = 1,
+        Pin2 = 2,
+        Pin3 = 3,
+        Pin4 = 4,
+        Pin5 = 5,
+        Pin6 = 6,
+        Pin7 = 7,
+        Pin8 = 8,
+        Pin9 = 9,
+        Pin10 = 10,
+        Pin11 = 11,
+        Pin12 = 12,
+        Pin13 = 13,
+        Pin14 = 14,
+        Pin15 = 15,
+    }
+
+    export enum ServoNum {
+        Servo1 = 1,
+        Servo2 = 2,
+        Servo3 = 3,
+        Servo4 = 4,
+        Servo5 = 5,
+        Servo6 = 6,
+        Servo7 = 7,
+        Servo8 = 8,
+        Servo9 = 9,
+        Servo10 = 10,
+        Servo11 = 11,
+        Servo12 = 12,
+        Servo13 = 13,
+        Servo14 = 14,
+        Servo15 = 15,
+        Servo16 = 16,
+    }
+
+
+
+    export class ServoConfigObject {
+        id: number;
+        pinNumber: number;
+        minOffset: number;
+        midOffset: number;
+        maxOffset: number;
+        position: number;
+    }
+
+    export const DefaultServoConfig = new ServoConfigObject();
+    DefaultServoConfig.pinNumber = -1
+    DefaultServoConfig.minOffset = 5
+    DefaultServoConfig.midOffset = 15
+    DefaultServoConfig.maxOffset = 25
+    DefaultServoConfig.position = 90
+
+    export class ServoConfig {
+        id: number;
+        pinNumber: number;
+        minOffset: number;
+        midOffset: number;
+        maxOffset: number;
+        position: number;
+        constructor(id: number, config: ServoConfigObject) {
+            this.id = id
+            this.init(config)
+        }
+
+        init(config: ServoConfigObject) {
+            this.pinNumber = config.pinNumber > -1 ? config.pinNumber : this.id - 1
+            this.setOffsetsFromFreq(config.minOffset, config.maxOffset, config.midOffset)
+            this.position = -1
+        }
+
+        debug() {
+            const params = this.config()
+
+            for (let j = 0; j < params.length; j = j + 2) {
+                debug(`Servo[${this.id}].${params[j]}: ${params[j + 1]}`)
+            }
+        }
+
+        setOffsetsFromFreq(startFreq: number, stopFreq: number, midFreq: number = -1): void {
+            this.minOffset = startFreq // calcFreqOffset(startFreq)
+            this.maxOffset = stopFreq // calcFreqOffset(stopFreq)
+            this.midOffset = midFreq > -1 ? midFreq : ((stopFreq - startFreq) / 2) + startFreq
+        }
+
+        config(): string[] {
+            return [
+                'id', this.id.toString(),
+                'pinNumber', this.pinNumber.toString(),
+                'minOffset', this.minOffset.toString(),
+                'maxOffset', this.maxOffset.toString(),
+                'position', this.position.toString(),
+            ]
+        }
+    }
+
+    export class ChipConfig {
+        address: number;
+        servos: ServoConfig[];
+        freq: number;
+        constructor(address: number = 0x40, freq: number = 50) {
+            this.address = address
+            this.servos = [
+                new ServoConfig(1, DefaultServoConfig),
+                new ServoConfig(2, DefaultServoConfig),
+                new ServoConfig(3, DefaultServoConfig),
+                new ServoConfig(4, DefaultServoConfig),
+                new ServoConfig(5, DefaultServoConfig),
+                new ServoConfig(6, DefaultServoConfig),
+                new ServoConfig(7, DefaultServoConfig),
+                new ServoConfig(8, DefaultServoConfig),
+                new ServoConfig(9, DefaultServoConfig),
+                new ServoConfig(10, DefaultServoConfig),
+                new ServoConfig(11, DefaultServoConfig),
+                new ServoConfig(12, DefaultServoConfig),
+                new ServoConfig(13, DefaultServoConfig),
+                new ServoConfig(14, DefaultServoConfig),
+                new ServoConfig(15, DefaultServoConfig),
+                new ServoConfig(16, DefaultServoConfig)
+            ]
+            this.freq = freq
+            init(address, freq)
+        }
+    }
+
+    export const chips: ChipConfig[] = []
+
+    function calcFreqPrescaler(freq: number): number {
+        return (25000000 / (freq * chipResolution)) - 1;
+    }
+
+    function stripHexPrefix(str: string): string {
+        if (str.length === 2) {
+            return str
+        }
+        if (str.substr(0, 2) === '0x') {
+            return str.substr(-2, 2)
+        }
+        return str
+    }
+
+    function write(chipAddress: number, register: number, value: number): void {
+        const buffer = pins.createBuffer(2)
+        buffer[0] = register
+        buffer[1] = value
+        pins.i2cWriteBuffer(chipAddress, buffer, false)
+    }
+
+    export function getChipConfig(address: number): ChipConfig {
+        for (let i = 0; i < chips.length; i++) {
+            if (chips[i].address === address) {
+                debug(`Returning chip ${i}`)
+                return chips[i]
+            }
+        }
+        debug(`Creating new chip for address ${address}`)
+        const chip = new ChipConfig(address)
+        const index = chips.length
+        chips.push(chip)
+        return chips[index]
+    }
+
+    function calcFreqOffset(freq: number, offset: number) {
+        return ((offset * 1000) / (1000 / freq) * chipResolution) / 10000
+    }
+
+
+    function degrees180ToPWM(freq: number, degrees: number, offsetStart: number, offsetEnd: number): number {
+        // Calculate the offset of the off point in the freq
+        offsetEnd = calcFreqOffset(freq, offsetEnd)
+        offsetStart = calcFreqOffset(freq, offsetStart)
+        const spread: number = offsetEnd - offsetStart
+        const calcOffset: number = ((degrees * spread) / 180) + offsetStart
+        // Clamp it to the bounds
+        return Math.max(offsetStart, Math.min(offsetEnd, calcOffset))
+    }
+
+    /**
+     * Used to move the given servo to the specified degrees (0-180) connected to the KSB038
+     * @param chipAddress [64-125] The I2C address of your KSB038; eg: 64
+     * @param servoNum The number (1-16) of the servo to move
+     * @param degrees The degrees (0-180) to move the servo to
+     */
+    //% block
+    export function setServoPosition(servoNum: ServoNum = 1, degrees: number, chipAddress: number = 0x40): void {
+        const chip = getChipConfig(chipAddress)
+        servoNum = Math.max(1, Math.min(16, servoNum))
+        degrees = Math.max(0, Math.min(180, degrees))
+        const servo: ServoConfig = chip.servos[servoNum - 1]
+        const pwm = degrees180ToPWM(chip.freq, degrees, servo.minOffset, servo.maxOffset)
+        servo.position = degrees
+        debug(`setServoPosition(${servoNum}, ${degrees}, ${chipAddress})`)
+        debug(`  servo.pinNumber ${servo.pinNumber}`)
+        debug(`  servo.minOffset ${servo.minOffset}`)
+        debug(`  servo.maxOffset ${servo.maxOffset}`)
+        debug(`  pwm ${pwm}`)
+        servo.debug()
+        return setPinPulseRange(servo.pinNumber, 0, pwm, chipAddress)
+    }
+
+    /**
+     * Used to set the rotation speed of a continous rotation servo from -100% to 100%
+     * @param chipAddress [64-125] The I2C address of your KSB038; eg: 64
+     * @param servoNum The number (1-16) of the servo to move
+     * @param speed [-100-100] The speed (-100-100) to turn the servo at
+     */
+    //% block
+    export function setCRServoPosition(servoNum: ServoNum = 1, speed: number, chipAddress: number = 0x40): void {
+        debug(`setCRServoPosition(${servoNum}, ${speed}, ${chipAddress})`)
+        const chip = getChipConfig(chipAddress)
+        const freq = chip.freq
+        servoNum = Math.max(1, Math.min(16, servoNum))
+        const servo: ServoConfig = chip.servos[servoNum - 1]
+        const offsetStart = calcFreqOffset(freq, servo.minOffset)
+        const offsetMid = calcFreqOffset(freq, servo.midOffset)
+        const offsetEnd = calcFreqOffset(freq, servo.maxOffset)
+        if (speed === 0) {
+            return setPinPulseRange(servo.pinNumber, 0, offsetMid, chipAddress)
+        }
+        const isReverse: boolean = speed < 0
+        debug(isReverse ? 'Reverse' : 'Forward')
+        const spread = isReverse ? offsetMid - offsetStart : offsetEnd - offsetMid
+        debug(`Spread ${spread}`)
+        servo.position = speed
+        speed = Math.abs(speed)
+        const calcOffset: number = ((speed * spread) / 100)
+        debug(`Offset ${calcOffset}`)
+        debug(`min ${offsetStart}`)
+        debug(`mid ${offsetMid}`)
+        debug(`max ${offsetEnd}`)
+        const pwm = isReverse ? offsetMid - calcOffset : offsetMid + calcOffset
+        debug(`pwm ${pwm}`)
+        return setPinPulseRange(servo.pinNumber, 0, pwm, chipAddress)
+    }
+
+
+
+    export function setDebug(debugEnabled: boolean): void {
+        _DEBUG = debugEnabled
+    }
+}
