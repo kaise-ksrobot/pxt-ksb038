47769f43676f8c854ef1b470afbb69238b78f4b8
ksrobot <arthur@kaise.com.tw>
2018-04-22 13:19:31 +0800

    Update main.ts

--- tests.ts
+++ tests.ts
@@ -1,15 +1,1 @@
-let controller = KSB038.chipAddress("0x40")
-input.onButtonPressed(Button.A, () => {
-    KSB038.setServoPosition(KSB038.ServoNum.Servo1, 0, controller)
-    basic.showString("A")
-})
-input.onButtonPressed(Button.B, () => {
-    KSB038.setServoPosition(KSB038.ServoNum.Servo1, 180, controller)
-    basic.showString("B")
-})
-input.onButtonPressed(Button.AB, () => {
-    KSB038.setServoPosition(KSB038.ServoNum.Servo1, 90, controller)
-    basic.showString("C")
-})
-basic.showNumber(controller)
-KSB038.init(controller, 60)
+tests
