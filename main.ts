aad7cdb2e9a86b832ed15b40d69a1ab4b75c4d52
kaise-ksrobot <arthur@kaise.com.tw>
2018-04-27 20:39:09 +0800

    V0.005

--- main.ts
+++ main.ts
@@ -18,41 +18,21 @@ namespace KSB038 {
     const modeRegister1Default = 0x01
     const modeRegister2 = 0x01 // MODE2
     const modeRegister2Default = 0x04
+    
     const sleep = modeRegister1Default | 0x10; // Set sleep bit to 1
     const wake = modeRegister1Default & 0xEF; // Set sleep bit to 0
     const restart = wake | 0x80; // Set restart bit to 1
+    
     const allChannelsOnStepLowByte = 0xFA // ALL_LED_ON_L
     const allChannelsOnStepHighByte = 0xFB // ALL_LED_ON_H
     const allChannelsOffStepLowByte = 0xFC // ALL_LED_OFF_L
     const allChannelsOffStepHighByte = 0xFD // ALL_LED_OFF_H
-    const PinRegDistance = 4
-    const channel0OnStepLowByte = 0x06 // LED0_ON_L
-    const channel0OnStepHighByte = 0x07 // LED0_ON_H
-    const channel0OffStepLowByte = 0x08 // LED0_OFF_L
-    const channel0OffStepHighByte = 0x09 // LED0_OFF_H
+    
+
 
     const hexChars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f']
 
 
-
-    export enum PinNum {
-        Pin0 = 0,
-        Pin1 = 1,
-        Pin2 = 2,
-        Pin3 = 3,
-        Pin4 = 4,
-        Pin5 = 5,
-        Pin6 = 6,
-        Pin7 = 7,
-        Pin8 = 8,
-        Pin9 = 9,
-        Pin10 = 10,
-        Pin11 = 11,
-        Pin12 = 12,
-        Pin13 = 13,
-        Pin14 = 14,
-        Pin15 = 15,
-    }
 
     export enum ServoNum {
         Servo1 = 1,
@@ -73,24 +53,7 @@ namespace KSB038 {
         Servo16 = 16,
     }
 
-    export enum LEDNum {
-        LED1 = 1,
-        LED2 = 2,
-        LED3 = 3,
-        LED4 = 4,
-        LED5 = 5,
-        LED6 = 6,
-        LED7 = 7,
-        LED8 = 8,
-        LED9 = 9,
-        LED10 = 10,
-        LED11 = 11,
-        LED12 = 12,
-        LED13 = 13,
-        LED14 = 14,
-        LED15 = 15,
-        LED16 = 16,
-    }
+
 
     export class ServoConfigObject {
         id: number;
@@ -221,51 +184,7 @@ namespace KSB038 {
         return ((offset * 1000) / (1000 / freq) * chipResolution) / 10000
     }
 
-    /**
-     * Used to set the pulse range (0-4095) of a given pin on the KSB038
-     * @param chipAddress [64-125] The I2C address of your KSB038; eg: 64
-     * @param pinNumber The pin number (0-15) to set the pulse range on
-     * @param onStep The range offset (0-4095) to turn the signal on
-     * @param offStep The range offset (0-4095) to turn the signal off
-     */
-    //% block advanced=true
-    export function setPinPulseRange(pinNumber: PinNum = 0, onStep: number = 0, offStep: number = 2048, chipAddress: number = 0x40): void {
-        pinNumber = Math.max(0, Math.min(15, pinNumber))
-        const buffer = pins.createBuffer(2)
-        const pinOffset = PinRegDistance * pinNumber
-        onStep = Math.max(0, Math.min(4095, onStep))
-        offStep = Math.max(0, Math.min(4095, offStep))
-
-        debug(`setPinPulseRange(${pinNumber}, ${onStep}, ${offStep}, ${chipAddress})`)
-        debug(`  pinOffset ${pinOffset}`)
-
-        // Low byte of onStep
-        write(chipAddress, pinOffset + channel0OnStepLowByte, onStep & 0xFF)
-
-        // High byte of onStep
-        write(chipAddress, pinOffset + channel0OnStepHighByte, (onStep >> 8) & 0x0F)
-
-        // Low byte of offStep
-        write(chipAddress, pinOffset + channel0OffStepLowByte, offStep & 0xFF)
-
-        // High byte of offStep
-        write(chipAddress, pinOffset + channel0OffStepHighByte, (offStep >> 8) & 0x0F)
-    }
-
-    /**
-     * Used to set the duty cycle (0-100) of a given led connected to the KSB038
-     * @param chipAddress [64-125] The I2C address of your KSB038; eg: 64
-     * @param ledNumber The number (1-16) of the LED to set the duty cycle on
-     * @param dutyCycle The duty cycle (0-100) to set the LED to
-     */
-    //% block
-    export function setLedDutyCycle(ledNum: LEDNum = 1, dutyCycle: number, chipAddress: number = 0x40): void {
-        ledNum = Math.max(1, Math.min(16, ledNum))
-        dutyCycle = Math.max(0, Math.min(100, dutyCycle))
-        const pwm = (dutyCycle * (chipResolution - 1)) / 100
-        debug(`setLedDutyCycle(${ledNum}, ${dutyCycle}, ${chipAddress})`)
-        return setPinPulseRange(ledNum - 1, 0, pwm, chipAddress)
-    }
+   
 
     function degrees180ToPWM(freq: number, degrees: number, offsetStart: number, offsetEnd: number): number {
         // Calculate the offset of the off point in the freq
@@ -335,26 +254,6 @@ namespace KSB038 {
         return setPinPulseRange(servo.pinNumber, 0, pwm, chipAddress)
     }
 
-    /**
-     * Used to set the range in centiseconds (milliseconds * 10) for the pulse width to control the connected servo
-     * @param chipAddress [64-125] The I2C address of your KSB038; eg: 64
-     * @param servoNum The number (1-16) of the servo to move; eg: 1
-     * @param minTimeCs The minimum centiseconds (0-1000) to turn the servo on; eg: 5
-     * @param maxTimeCs The maximum centiseconds (0-1000) to leave the servo on for; eg: 25
-     * @param midTimeCs The mid (90 degree for regular or off position if continuous rotation) for the servo; eg: 15
-     */
-    //% block advanced=true
-    export function setServoLimits(servoNum: ServoNum = 1, minTimeCs: number = 5, maxTimeCs: number = 2.5, midTimeCs: number = -1, chipAddress: number = 0x40): void {
-        const chip = getChipConfig(chipAddress)
-        servoNum = Math.max(1, Math.min(16, servoNum))
-        minTimeCs = Math.max(0, minTimeCs)
-        maxTimeCs = Math.max(0, maxTimeCs)
-        debug(`setServoLimits(${servoNum}, ${minTimeCs}, ${maxTimeCs}, ${chipAddress})`)
-        const servo: ServoConfig = chip.servos[servoNum - 1]
-        midTimeCs = midTimeCs > -1 ? midTimeCs : ((maxTimeCs - minTimeCs) / 2) + minTimeCs
-        debug(`midTimeCs ${midTimeCs}`)
-        return servo.setOffsetsFromFreq(minTimeCs, maxTimeCs, midTimeCs)
-    }
 
     /**
      * Used to setup the chip, will cause the chip to do a full reset and turn off all outputs.
@@ -383,14 +282,6 @@ namespace KSB038 {
         write(chipAddress, modeRegister1, restart)
     }
 
-    /**
-     * Used to reset the chip, will cause the chip to do a full reset and turn off all outputs.
-     * @param chipAddress [64-125] The I2C address of your KSB038; eg: 64
-     */
-    //% block
-    export function reset(chipAddress: number = 0x40): void {
-        return init(chipAddress, getChipConfig(chipAddress).freq);
-    }
 
     /**
      * Used to reset the chip, will cause the chip to do a full reset and turn off all outputs.
