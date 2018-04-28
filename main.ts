9045ab606aa6a73b2578b5e6035ebe470784b86c
kaise-ksrobot <arthur@kaise.com.tw>
2018-04-28 22:11:10 +0800

    V0.0.5

--- main.ts
+++ main.ts
@@ -3,66 +3,70 @@
  */
 //% weight=10 color=#0000f0 icon="\uf085" block="KSB038"
 namespace KSB038 {
+    
+    const SERVOMIN = 113 // this is the 'minimum' pulse length count (out of 4096)
+    const SERVOMAX = 600 // this is the 'maximum' pulse length count (out of 4096)
     const IIC_ADDRESS = 0x40
     const MODE1 = 0x00
     const PRESCALE = 0xFE
     const LED0_ON_L = 0x06
- 	
+    
     export enum ServoNum {
-        Servo0 = 0,
-        Servo1 = 1,
-        Servo2 = 2,
-        Servo3 = 3,
-        Servo4 = 4,
-        Servo5 = 5,
-        Servo6 = 6,
-        Servo7 = 7,
-        Servo8 = 8,
-        Servo9 = 9,
-        Servo10 = 10,
-        Servo11 = 11,
-        Servo12 = 12,
-        Servo13 = 13,
-        Servo14 = 14,
-        Servo15 = 15,
+        S0 = 0,
+        S1 = 1,
+        S2 = 2,
+        S3 = 3,
+        S4 = 4,
+        S5 = 5,
+        S6 = 6,
+        S7 = 7,
+        S8 = 8,
+        S9 = 9,
+        S10 = 10,
+        S11 = 11,
+        S12 = 12,
+        S13 = 13,
+        S14 = 14,
+        S15 = 15,
         
     }
 
 
     let initialized = false
 	
-    function i2cwrite(reg: number, value: number) {
+    function i2c_write(reg: number, value: number) {
 		let buf = pins.createBuffer(2)
         buf[0] = reg
         buf[1] = value
         pins.i2cWriteBuffer(IIC_ADDRESS, buf)
     }
 
-	function i2cread(reg: number){
+	function i2c_read(reg: number){
 		pins.i2cWriteNumber(IIC_ADDRESS, reg, NumberFormat.UInt8BE);
         let val = pins.i2cReadNumber(IIC_ADDRESS, NumberFormat.UInt8BE);
         return val;
 	}
 
     function init(): void {
-		i2cwrite(MODE1, 0x00)
+		i2c_write(MODE1, 0x00)
         let freq=50;
         // Constrain the frequency
-        let prescaleval = 25000000;
-        prescaleval /= 4096;
-        prescaleval /= freq;
+        let prescaleval = 25000000/4096/freq;
         prescaleval -= 1;
         let prescale = prescaleval; //Math.Floor(prescaleval + 0.5);
-        let oldmode = i2cread(MODE1);        
+        let oldmode = i2c_read(MODE1);        
         let newmode = (oldmode & 0x7F) | 0x10; // sleep
-        i2cwrite(MODE1, newmode); // go to sleep
-        i2cwrite(PRESCALE, prescale); // set the prescaler
-        i2cwrite(MODE1, oldmode);
+        i2c_write(MODE1, newmode); // go to sleep
+        i2c_write(PRESCALE, prescale); // set the prescaler
+        i2c_write(MODE1, oldmode);
         control.waitMicros(5000);
-        i2cwrite(MODE1, oldmode | 0xa1);
+        i2c_write(MODE1, oldmode | 0xa1);
         initialized = true
     }
-	
+	function servo_map(x: number, in_min: number, in_max: number, out_min: number, out_max: number)
+    {
+        return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+    }
 	
 	/**
      * Used to move the given servo to the specified degrees (0-180) connected to the KSB038
@@ -75,8 +79,10 @@ namespace KSB038 {
 			init()
 		}
 		// 50hz: 20,000 us
-        let v_us = (degree*1800/180+600) // 0.6 ~ 2.4
-        let value = v_us*4096/20000
+        //let servo_timing = (degree*1800/180+600) // 0.6 ~ 2.4
+        //let pulselen = servo_timing*4096/20000
+
+        let pulselen = servo_map(degree, 0, 180, SERVOMIN, SERVOMAX);
         
         if (channel < 0 || channel > 15)
             return;
@@ -85,8 +91,8 @@ namespace KSB038 {
         buf[0] = LED0_ON_L + 4 * channel;
         buf[1] = 0;
         buf[2] = (0>>8);
-        buf[3] = value & 0xff;
-        buf[4] = (value>>8) & 0xff;
+        buf[3] = pulselen & 0xff;
+        buf[4] = (pulselen>>8) & 0xff;
         pins.i2cWriteBuffer(IIC_ADDRESS, buf);
     }
 	
